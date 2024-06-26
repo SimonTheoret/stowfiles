e70ec12e5c2a7fb4284ecc717851d59396e6ee56
Simon Théorêt <104704678+SimonTheoret@users.noreply.github.com>
2024-06-26 16:18:35 -0400

    Update dap-ui.lua from gpix3

--- .config/nvim/lua/plugins/dap-ui.lua
+++ .config/nvim/lua/plugins/dap-ui.lua
@@ -1,23 +1,48 @@
-return {
-    {
-        'theHamsta/nvim-dap-virtual-text',
-        config = function()
-            require("nvim-dap-virtual-text").setup()
-        end
-    },
-    {
-        "rcarriga/nvim-dap-ui",
-        dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
-        keys = {
-            { "<leader>duo", function()
-                require("dapui").open()
-            end, desc="Open dapui"},
-
-            { "<leader>duc", function()
-                require("dapui").close()
-            end, desc="Close dapui"},
-            { "<leader>dut", function()
-                require("dapui").toggle()
-            end, desc="Toggle dapui"},
-        },
-    } }
+return {                                                                                                                                                                                                                                        
+    {                                                                                                                                                                                                                                           
+        'theHamsta/nvim-dap-virtual-text',                                                                                                                                                                                                      
+        config = function()                                                                                                                                                                                                                     
+            require("nvim-dap-virtual-text").setup(                                                                                                                                                                                             
+                { enabled = false, }                                                                                                                                                                                                            
+            )                                                                                                                                                                                                                                   
+            local dap, dapui = require("dap"), require("dapui")                                                                                                                                                                                 
+            dap.listeners.before.attach.dapui_config = function()                                                                                                                                                                               
+                dapui.open()                                                                                                                                                                                                                    
+            end                                                                                                                                                                                                                                 
+            dap.listeners.before.launch.dapui_config = function()                                                                                                                                                                               
+                dapui.open()                                                                                                                                                                                                                    
+            end                                                                                                                                                                                                                                 
+            dap.listeners.before.event_terminated.dapui_config = function()                                                                                                                                                                     
+                dapui.close()                                                                                                                                                                                                                   
+            end                                                                                                                                                                                                                                 
+            dap.listeners.before.event_exited.dapui_config = function()                                                                                                                                                                         
+                dapui.close()                                                                                                                                                                                                                   
+            end                                                                                                                                                                                                                                 
+        end,                                                                                                                                                                                                                                    
+        keys = {                                                                                                                                                                                                                                
+            { "<leader>dvt", ":DapVirtualTextToggle<CR>", desc = "DAP virtual text toggle" }                                                                                                                                                    
+        }                                                                                                                                                                                                                                       
+    },                                                                                                                                                                                                                                          
+    {                                                                                                                                                                                                                                           
+        "rcarriga/nvim-dap-ui",                                                                                                                                                                                                                 
+        config = function()                                                                                                                                                                                                                     
+            require('dapui').setup()                                                                                                                                                                                                            
+        end,                                                                                                                                                                                                                                    
+        dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },                                                                                                                                                                    
+        keys = {                                                                                                                                                                                                                                
+            {                                                                                                                                                                                                                                   
+                "<leader>dut",                                                                                                                                                                                                                  
+                function()                                                                                                                                                                                                                      
+                    require("dapui").toggle()                                                                                                                                                                                                   
+                end,                                                                                                                                                                                                                            
+                desc = "Toggle dapui"                                                                                                                                                                                                           
+            },                                                                                                                                                                                                                                  
+            {                                                                                                                                                                                                                                   
+                "<leader>due",                                                                                                                                                                                                                  
+                function()                                                                                                                                                                                                                      
+                    require("dapui").eval()                                                                                                                                                                                                     
+                end,                                                                                                                                                                                                                            
+                desc = "Eval dapui"                                                                                                                                                                                                             
+            }                                                                                                                                                                                                                                   
+        },                                                                                                                                                                                                                                      
+    } }  
