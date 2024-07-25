06507d5ccbc9d04beeebcf88f9149b482a7128b6
Simon Théorêt <simonteoret@hotmail.com>
2024-07-24 20:35:45 -0400

    new toys

---
+++ .config/emacs/.dir-locals.el
@@ -0,0 +1,9 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . (
+	 (eval . (setq consult-ripgrep-args (concat consult-ripgrep-args " -L --hidden "))
+	       )
+	 )
+))
+
---
+++ .config/emacs/conf/projdeploy.el
@@ -0,0 +1,6 @@
+(use-package async)
+(use-package ssh-deploy
+  :config
+  (ssh-deploy-line-mode)
+  (ssh-deploy-add-menu)
+)
